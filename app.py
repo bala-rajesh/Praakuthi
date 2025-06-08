b4205f1a82ee1db885462e48934330475bdc67b9
bala-rajesh <143264835+bala-rajesh@users.noreply.github.com>
2025-06-08 09:40:36 +0530

    Project Upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, render_template, jsonify, request
+from model import model_prediction
+from markupsafe import Markup
+import utils
+
+app = Flask(__name__)
+
+num_classes = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            file = request.files['file']
+            img = file.read()
+            prediction = model_prediction(img)
+            disease = num_classes[prediction]
+            print(disease)
+            res = Markup(utils.disease_dic.get(disease, "Unknown disease"))
+            return render_template('display.html', status=200, result=res)
+        except:
+            pass
+    return render_template('index.html', status=500, res="Internal Server Error")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
